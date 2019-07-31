0f6d371828879abaa8ff4bbcc4b7be33ada91193
Rodion Kharabet <pensioneer4ik@gmail.com>
2019-08-01 02:25:36 +0300

    Add WebSpeechAPI with basic grammar

---
+++ src/ShoppingListArduino/ShoppingListArduino/wwwroot/js/WebSpeech.js
@@ -0,0 +1,66 @@
+﻿var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var colors = ['aqua', 'azure', 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+
+var grammar = `#JSGF V1.0;
+
+grammar voicecommands;
+
+ public <command> = <action> <object>;
+<action> = /10/ add |/2/ remove |/1/ delete |/1/ throw;
+<object> = колбаса | молоко | хлеб ;`;
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+//recognition.continuous = false;
+recognition.lang = 'ru-ru';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+var diagnostic = document.querySelector('.output');
+var bg = document.querySelector('html');
+var hints = document.querySelector('.hints');
+var recordButton = document.querySelector('.start-record');
+var colorHTML = '';
+hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try ' + colorHTML + '.';
+
+$(recordButton).on("click",
+    function() {
+        recognition.start();
+        console.log('Ready to receive a color command.');
+    });
+
+recognition.onresult = function (event) {
+    // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+    // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+    // It has a getter so it can be accessed like an array
+    // The [last] returns the SpeechRecognitionResult at the last position.
+    // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+    // These also have getters so they can be accessed like arrays.
+    // The [0] returns the SpeechRecognitionAlternative at position 0.
+    // We then return the transcript property of the SpeechRecognitionAlternative object
+
+    var last = event.results.length - 1;
+    var color = event.results[last][0].transcript;
+
+    diagnostic.textContent = 'Result received: ' + color + '.';
+    bg.style.backgroundColor = color;
+    console.log('Confidence: ' + event.results[0][0].confidence);
+}
+
+recognition.onspeechend = function () {
+    recognition.stop();
+}
+
+recognition.onnomatch = function (event) {
+    diagnostic.textContent = "I didn't recognise that color.";
+}
+
+recognition.onerror = function (event) {
+    diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+}
